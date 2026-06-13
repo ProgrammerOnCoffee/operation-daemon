341ee49806dac283d40161b7c165fe4835d4a1f0
appellatebaton0 <145923930+appellatebaton0@users.noreply.github.com>
2026-06-12 20:10:56 -0500

    v0.3 - Example Effect
    Made a quick example effect.

--- scripts/effect.gd
+++ scripts/effect.gd
@@ -6,27 +6,28 @@
 # let you get the single-value ones like normal variables,
 # but none can be set.
 
-## The name of the effect.
 var effect_name :String: get = _get_effect_name
+## The name of the effect.
 @abstract func _get_effect_name() -> String
-## The description of the effect.
+
 var description :String: get = _get_description
+## The description of the effect.
 @abstract func _get_description() -> String
 
-## The identifier for the effect. Used to apply effects.
 var modification_type :String: get = _get_modification_type
+## The identifier for the effect. Used to apply effects.
 @abstract func _get_modification_type() -> String
 
-## Whether this effect is applied to the attacker or attackee
 var target_type :Module.TARGET: get = _get_target_type
+## Whether this effect is applied to the attacker or attackee
 @abstract func _get_target_type() -> Module.TARGET
 
-## The base value of the effect. Can be changed via Modifiers.
 var base :float: get = _get_base
+## The base value of the effect. Can be changed via Modifiers.
 @abstract func _get_base() -> float 
 
-## The final value after all the modifiers are applied. Use this for apply_effect.
 var value:float
+## The final value after all the modifiers are applied. Use this for apply_effect.
 
 ## Apply this effect to a target. Ran by that target.
 @abstract func apply_effect(target:Node) -> bool
