f95d83686e173868bec074e540f7062ecf93232e
appellatebaton0 <145923930+appellatebaton0@users.noreply.github.com>
2026-06-17 10:40:18 -0500

    Music Driver Update
    Updated the music driver to the new specifications - half-bar snap and bar-long cross-fade between tracks.

--- scripts/music_driver.gd
+++ scripts/music_driver.gd
@@ -4,10 +4,14 @@ class_name MusicDriver extends AudioStreamPlayer
 @export var tracks:Dictionary[String, AudioStreamOggVorbis]
 
 var bpm:float
-var bar_beats:int
+var bar_beats:float
 
-var beat:float
-var bar:int
+var beat:float:
+	set(to):
+		beat_delta = abs(beat - to)
+		beat = to
+var beat_delta:float
+var bar:float
 
 var current_stream:int = 0
 var stream_buffer:int = -1
@@ -39,20 +43,37 @@ func _ready() -> void:
 	
 	Global.request_track_transition.connect(buffer_stream_name)
 
-func _process(_delta: float) -> void:
+func _process(_delta: float) -> void: if stream is AudioStreamSynchronized:
+	
+	## Debug to allow switching track w/ an input.
+	#if Input.is_action_just_pressed("qte_press"): buffer_stream(1 - current_stream)
+	
+	#print("S2 ",  (get_playback_position() +  AudioServer.get_time_since_last_mix() - AudioServer.get_output_latency()) * bpm / 60)
 	beat = (get_playback_position() +  AudioServer.get_time_since_last_mix() - AudioServer.get_output_latency()) * bpm / 60
-	bar = floori(beat / bar_beats)
+	bar = beat / bar_beats
+	
+	#print(bar)
 	
 	# The only way the start of the transition is over 1u away
 	# is if the track just looped.
 	if abs(transition_start_bar - bar) > 1:
 		transition_start_bar = ceil(bar)
 	
-	# Resolve
+	# Resolve buffers
 	if stream_buffer != -1 and transition_start_bar != -1 and bar >= transition_start_bar:
-		_set_stream_to(stream_buffer)
+		#print("RES. BUFF")
+		current_stream = stream_buffer
 		stream_buffer = -1
 		transition_start_bar = -1
+	
+	# Manage track volumes for cross-fading. Takes 1 bar, using bar_delta.
+	#print("BD\t", beat_delta)
+	for i in stream.stream_count:
+		#stream.set_sync_stream_volume(i, 0.0)
+		#print(i,"=\t",stream.get_sync_stream_volume(i))
+		var new_volume = move_toward(db_to_linear(stream.get_sync_stream_volume(i)), (i == current_stream) as int, beat_delta / bar_beats) 
+		#print(i,(i == current_stream) as int,":\t",db_to_linear(stream.get_sync_stream_volume(i)),"\t", new_volume)
+		stream.set_sync_stream_volume(i, linear_to_db(new_volume))
 
 func _set_stream_to(index:int): if stream is AudioStreamSynchronized:
 	current_stream = index
@@ -64,7 +85,8 @@ func buffer_stream(index:int):
 	stream_buffer = index
 	
 	if transition_start_bar == -1:
-		transition_start_bar = ceil(bar)
+		# Snap the transition start to the nearest half-bar after this.
+		transition_start_bar = ceil(bar * 2) / 2
 
 func buffer_stream_name(from_name:String): buffer_stream(get_stream_index(from_name))
 
