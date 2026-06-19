cadebf561e95cade55da858d474e2047b15320bc
appellatebaton0 <145923930+appellatebaton0@users.noreply.github.com>
2026-06-19 18:01:36 -0500

    Forgot To Save
    take this single-line-change with you!

--- scripts/laboratory.gd
+++ scripts/laboratory.gd
@@ -58,6 +58,8 @@ func _process(delta: float) -> void:
 func _start_pressed() -> void: 
 	# Reset the current act.
 	Global.act = 0
+	Global.daemon_research.clear()
+	
 	# Reset the player's health.
 	PlayerData.health = PlayerData.max_health
 	
