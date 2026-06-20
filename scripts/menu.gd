e1637f24d7aa00ca043634a308c73250c95edcf0
ProgrammerOnCoffee <171874668+ProgrammerOnCoffee@users.noreply.github.com>
2026-06-20 15:22:37 -0400

    Update menu transitions Tweaked in order to make slides not be hidden abruptly and to fix issues with other aspect ratios

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -18,19 +18,39 @@ func _ready() -> void:
 		$QuitButton.show()
 
 
-func transition(from:NodePath, to:NodePath):
+func transition(from_path: NodePath, to_path: NodePath):
 	if transition_player.is_playing() or is_transitioning: return
 	
-	var from_name := get_node(from).name
-	var to_name := get_node(to).name
+	var from_node := get_node(from_path) as Control
+	var from_name := from_node.name
+	var to_node := get_node(to_path) as Control
+	var to_name := to_node.name
+	
+	const OFFSET = 256
+	is_transitioning = true
+	#TransitionManager.bar_count = 48
+	TransitionManager.fade(from_node)
+	TransitionManager.fade(to_node, true)
+	#TransitionManager.transition(from_node, to_node)
+	var tween := create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC).set_parallel()
+	#to_node.set_anchors_and_offsets_preset(PRESET_FULL_RECT)
+	
+	var travel_dir := +1 if (to_name == &"MainMenu" and from_name != &"Credits") or to_name == &"Credits" else -1
+	to_node.position.x = -travel_dir * OFFSET * 2
+	to_node.modulate.a = 0.0
+	
+	tween.tween_property(from_node, ^":position:x", travel_dir * OFFSET * 2, 1.0).as_relative()
+	tween.tween_property(from_node, ^":modulate:a", 0.0, 0.75)
+	tween.tween_property(to_node, ^":position:x", 0 if to_name == &"MainMenu" else -travel_dir * OFFSET, 1.0)
+	tween.tween_property(to_node, ^":modulate:a", 1.0, 0.75).set_delay(0.25)
+	tween.finished.connect(set.bind(&"is_transitioning", false))
+	TransitionManager.bar_count = 24
 	
 	if transition_player.has_animation(from_name + "<>" + to_name):
 		transition_player.play(from_name + "<>" + to_name)
 	elif transition_player.has_animation(to_name + "<>" + from_name):
 		transition_player.play(to_name + "<>" + from_name, -1, -1.0, true)
-	else: # No fancy transitions... just use the TM ig... :(
-		TransitionManager.transition_screen(get_node(from), get_node(to))
-	
+
 
 func _on_start_pressed() -> void:
 	TransitionManager.transition_screen($MainMenu, $Laboratory)
