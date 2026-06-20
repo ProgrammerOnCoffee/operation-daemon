a0e34d345f6679fbb4e117c6d4d6d4f3330a4904
ProgrammerOnCoffee <171874668+ProgrammerOnCoffee@users.noreply.github.com>
2026-06-20 09:21:31 -0400

    Remove file extension when setting effect type in `Modifier._get_effect_type`

--- scripts/modifiers/overcharge_modifier.gd
+++ scripts/modifiers/overcharge_modifier.gd
@@ -1,7 +1,7 @@
 extends Modifier
 ## A test modifier that returns random values.
 
-func _get_effect_type() -> Effect: return Effect.all_effects["overcharge_effect.gd"].new()
+func _get_effect_type() -> Effect: return Effect.all_effects["overcharge_effect"].new()
 
 # Ranging from -40% to 40%, w/o anything below an abs value of 10%
 func _get_new_percent() -> float: return 1 + ([-1,1].pick_random() * randf_range(0.1, 0.4))
